5452c285554c799c3d9fe87127976bb3e77034f1
aly_ssa00@outlook.com <aly_ssa00@outlook.com>
2021-04-07 20:03:54 +0800

    测试

---
+++ Add a new project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Add_a_new_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test1/unit test1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        
+        Console.WriteLine($"hello world");
+        Console.Read();
+
+
+    }
+   
+
+
+    }
+
+}
